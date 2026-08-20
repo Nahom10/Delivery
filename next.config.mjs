d7827b3f4533e69ad7d7a0462b584d6a7118fac7
Nahom <hnahom79@gmail.com>
2026-08-20 10:07:47 +0300

    Fix Vercel Next output directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: '.next-webpack',
   transpilePackages: ['@allfreshmart/core'],
   images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] }
 };
